1cdfbe5b25253edc0d8e914dc3b9c46ec1f87aaf
Omonlayé <idjaeulalie@gmail.com>
2024-03-28 17:24:18 +0100

    ajout

--- stream.py
+++ stream.py
@@ -1,11 +1,12 @@
 import streamlit as st
 
 def main():
-    st.title("Application WIADA 2023")
+    st.title("Application Omonlaye_WIADA 2023")
     user= st.text_input("Entrer votre nom : ")
     if st.button("Dis bonjour"):
         if user:
             st.write(f'Bonjour {user}')
+            st.write(f'Dites moi en quoi puis-je vous aider')
         else :
             st.write('Bonjour à tous')
 if __name__ == "__main__" :
