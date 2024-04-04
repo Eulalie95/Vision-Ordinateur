cb768d84ffccd67c269bd2740d86a2c299eb95e3
Omonlayé <idjaeulalie@gmail.com>
2024-04-04 13:26:07 +0100

    ajout

--- app.py
+++ app.py
@@ -58,5 +58,5 @@ if source_vid is not None:
                                use_column_width=True
                                )
             else:
-                vid_cap.release()
+                st.error("Failed to read frame. Exiting...")
                 break
